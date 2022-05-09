28aec5a0ec33333919951975a07558d3861d8ab3
unknown <bttsnam@gmail.com>
2022-05-10 00:03:00 +0900

    updare travis

--- set_env.sh
+++ set_env.sh
@@ -3,7 +3,7 @@
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Admin123
 export POSTGRES_HOST=udagram.co9ejycdogrl.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=udagram
+export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::my-825566562884-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=test
